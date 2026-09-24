9e42cc1b5126bf3f3870505e4660c13efd298e80
NL-Des <nathan33.travail@gmail.com>
2026-09-24 10:32:57 +0200

    Début du projet, ajout du guide et début du main.rs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RT
+Sujet github de l'exercice : https://github.com/01-edu/public/tree/master/subjects/rt
+Guide pour créer le moteur en rust : https://the-ray-tracing-road-to-rust.vercel.app/
---
+++ ray-tracing/src/main.rs
@@ -0,0 +1,25 @@
+fn main () {
+    // Image
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    // Render
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    // Double boucles qui va écrire les pixels de l'image.
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            // 0 c'est noir, 1 c'est blanc. On va donc faire un dégradé de couleur.
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64; // Red pour rouge.
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64; // Green pour vert.
+            let b = 0.25; // Blue pour bleu.
+
+            // Conversion des valeurs de 0-1 à 0-255 pour l'affichage.
+            let ir = (255.999 * r) as i32; // 
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
+}
